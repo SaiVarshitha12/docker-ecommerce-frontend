2b6230b8ce89224ea43dddb810d30edb5262c076
BHARATA SAI VARSHITHA <saivarshitha95@gmail.com>
2025-09-20 07:49:18 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-54-210-31-224.compute-1.amazonaws.com:8081/auth";
+const API_URL = "http://34.228.159.66:8081/auth";
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
   localStorage.setItem("token", response.data);
